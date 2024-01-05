1b0ff0239cc0a2139242189764d1a4aa04077a8f
yeoshuheng <yeoshuheng@gmail.com>
2024-01-06 03:09:57 +0800

    fix: call to wallet

--- client/components/UserHoldings.js
+++ client/components/UserHoldings.js
@@ -3,7 +3,7 @@ import { useWallet } from "@thirdweb-dev/react";
 import styles from "../styles/user.module.css";
 
 // Token IDs only work for Sepolia Testnet.
-const token_ids = [, // Current Network
+const token_ids = [
     "0x779877A7B0D9E8603169DdbD7836e478b4624789", // LINK
     "0x36160274B0ED3673E67F2CA5923560a7a0c523aa" // USDT
 ];
@@ -16,16 +16,11 @@ const UserHoldings = () => {
     useEffect(() => {
         setLoading(true);
         setOutput([]);
-        async function generateEntryComponents(tokens) {
-            const helper = (x) => wallet.getBalance(x).then(
-                (x) => {return x;}).catch((err) => {console.log(err);});
-            let ret = [];
-            for (let i = 0; i < tokens.length; i++) {
-                ret.push(helper(tokens[i]));
-            }
-            return Promise.all(ret);
-        };
-        generateEntryComponents(token_ids).then((entries) => {
+        Promise.all(token_ids.map(x => wallet.getBalance(x).then(
+            (x) => {return x;}).catch((err) => 
+            {console.log(err);
+                return (<div>Problem loading holdings...</div>)
+            }))).then((entries) => {
                 const processed = entries.map(x => (
                     <div key = {x.symbol}>
                             <div className = {styles.entryBox}>
